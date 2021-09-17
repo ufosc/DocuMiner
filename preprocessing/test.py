9a05ae84fcf09cf2e77f89b7f2d00fbab6b277fb
Patrick-Lapid <patrickklapid@gmail.com>
2021-09-16 22:11:15 -0400

    Allows multiple txt files

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -2,14 +2,22 @@ import nltk
 # nltk.download('punkt')
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.util import filestring
+usrInput = input("Enter file name: ")
+fileNames = [usrInput]
 
-fileName = input("Enter File Name: ")
+
+while usrInput != "":
+    fileNames.append(usrInput)
+    usrInput = input("Enter file name (ENTER to terminate): ")
+    
+
 try: 
-    with open(fileName, encoding='utf-8') as file:
-        text = file.read()
-        words = word_tokenize(text)
-        sentences = sent_tokenize(text)
-        print('Words: ', words, '\nSentences: ', sentences)
+    for fileName in fileNames:
+        with open(fileName, encoding='utf-8') as file:
+            text = file.read()
+            words = word_tokenize(text)
+            sentences = sent_tokenize(text)
+            print('Words: ', words, '\nSentences: ', sentences)
 
 except IOError as e:
     print("I/O error({0}): {1}".format(e.errno, e.strerror))

832bcb3e13d5db2b34805e5cfd7ec1ea387585fb
Patrick-Lapid <patrickklapid@gmail.com>
2021-09-17 21:47:20 -0400

    fix merge errors

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -1,7 +1,9 @@
-import nltk
-# nltk.download('punkt')
-from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.util import filestring
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+import os
+# nltk.download('punkt')
+
 usrInput = input("Enter file name: ")
 fileNames = [usrInput]
 
@@ -9,9 +11,9 @@ fileNames = [usrInput]
 while usrInput != "":
     fileNames.append(usrInput)
     usrInput = input("Enter file name (ENTER to terminate): ")
-    
 
-try: 
+
+try:
     for fileName in fileNames:
         with open(fileName, encoding='utf-8') as file:
             text = file.read()
@@ -23,4 +25,3 @@ except IOError as e:
     print("I/O error({0}): {1}".format(e.errno, e.strerror))
 except OSError:
     print(f"OS error trying to open {fileName}")
-

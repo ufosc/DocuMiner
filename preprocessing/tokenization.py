9a6ceecc67d61821de3728c93bfef5deb97e6f05
Patrick-Lapid <patrickklapid@gmail.com>
2021-09-26 15:51:18 -0400

    changed return values to strings

--- preprocessing/tokenization.py
+++ preprocessing/tokenization.py
@@ -15,18 +15,18 @@ def main():
     
     # UNCOMMENT FOR FUNCTIONS
 
-    #words, sentences = URL(words, sentences)
+    text = URL(words, sentences)
 
     # singleFile(fileNames)
     # words, sentences = tokenizeFiles(fileNames, path)
 
     # fileNames = multipleFiles(fileNames)
-    # words, sentences = tokenizeFiles(fileNames, path)
+    # text = tokenizeFiles(fileNames, path)
 
-    fileNames, path = directory(fileNames)
-    words, sentences = tokenizeFiles(fileNames, path)
+    # fileNames, path = directory(fileNames)
+    # text = tokenizeFiles(fileNames, path)
 
-    print('Words: ', words, '\nSentences: ', sentences)
+    print('Text: ', text)
 
 
 # Scrapes and Tokenizes txt from URL
@@ -40,21 +40,9 @@ def URL(words, sentences):
         sys.exit()
 
     raw = BeautifulSoup(html.text, 'html.parser').get_text()
+    print(raw)
 
-    searchWord = input(
-        "Enter search keyword (ENTER to parse entire html page): ")
-    wordTokens = word_tokenize(raw)
-    words = word_tokenize(raw)
-    sentences = sent_tokenize(raw)
-    # Turns text into nltk object, concordance searches and returns keyword w/text around it in array
-    if(searchWord != ""):
-        wordObj = nltk.Text(wordTokens)
-        # Prints out concordance to terminal (VISUAL), does not save to array
-        wordObj.concordance(searchWord)
-        # Saves concordance to array wrapped in an nltk object type
-        words = wordObj.concordance_list(searchWord, width=150)
-
-    return words, sentences
+    return raw
 
 
 # Pulls filenames from Directory
@@ -91,36 +79,33 @@ def multipleFiles(fileNames):
 
 # Tokenizes txt files, run after multipleFiles() or singleFile()
 def tokenizeFiles(fileNames, path):
-    words = []
-    sentences = []
+    textArray = []
     try:
         for fileName in fileNames:
+            text = ""
             if fileName.endswith(".txt"):
                 with open(os.path.join(path, fileName), encoding='utf-8') as file:
                     text = file.read()
-                    words.append(word_tokenize(text))
-                    sentences.append(sent_tokenize(text))
+                    textArray.append(text)
             
             elif fileName.endswith(".pdf"):
                 with pdfplumber.open(os.path.join(path, fileName)) as file:
-                    text = ""
                     pages = file.pages
                     for page in pages:
                         text += page.extract_text()
-                    words.append(word_tokenize(text))
-                    sentences.append(sent_tokenize(text))
+                    textArray.append(text)
+                    
             
             elif fileName.endswith(".docx"):
                 text = docx2txt.process(os.path.join(path, fileName))
-                words.append(word_tokenize(text))
-                sentences.append(sent_tokenize(text))
+                textArray.append(text)
 
     except IOError as e:
         print("I/O error({0}): {1}".format(e.errno, e.strerror))
     except OSError:
         print(f"OS error trying to open {fileName}")
 
-    return words, sentences
+    return textArray
 
 
 main()

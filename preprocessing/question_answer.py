9914cbe5aa10d0c443d567cc58609ca2222ce704
Andrew Whigham <awhigham9@gmail.com>
2022-01-11 13:42:47 -0500

    Remove extra whitespace and add docstring

--- preprocessing/question_answer.py
+++ preprocessing/question_answer.py
@@ -2,32 +2,26 @@ from transformers import pipeline
 from tokenization import singleFile
 import os
 
-
 def questionAnswer(fileNames, text):
-    
+    ''' Runs a question answering routine using terminal user input
+        fileNames - list of file names
+        text - text[i] is the text of the ith file in fileNames
+    '''
     context = None
-
     print(fileNames)
 
     while(context == None):
-
         if(len(fileNames) == 1):
             #If only one file, auto choose it
             context = text[0]
-
         else:
             option = int(input("Choose the file (0 for first, 1 for second...): "))
-
             try:
                 context = text[option]
-
             except IndexError:
                 print("Option entered greater than number of files inputted")
 
     nlp = pipeline("question-answering")
-
     question = input("Enter a question about the text: ")
-
     results = nlp(question=question, context=context)
-
     return results
